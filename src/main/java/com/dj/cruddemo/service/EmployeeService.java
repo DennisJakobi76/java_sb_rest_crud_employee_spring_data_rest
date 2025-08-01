fd161ecb93030ff63a8d7a2f2898e3d567e47ac9
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-08-01 15:01:18 +0200

    first commit

---
+++ src/main/java/com/dj/cruddemo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.dj.cruddemo.service;
+
+import com.dj.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+}
---
+++ src/main/java/com/dj/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,51 @@
+package com.dj.cruddemo.service;
+
+import com.dj.cruddemo.dao.EmployeeRepository;
+import com.dj.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
+        employeeRepository = theEmployeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        Optional<Employee> result = employeeRepository.findById(theId);
+
+        Employee theEmployee = null;
+        if (result.isPresent()) {
+            theEmployee = result.get();
+        }
+        else {
+            // we didn`t find the employee
+            throw new RuntimeException("Employee not found wit id: " + theId);
+        }
+
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeRepository.save(theEmployee);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        employeeRepository.deleteById(theId);
+    }
+}
